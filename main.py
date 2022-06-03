aa559ae7d5479bd1dada4289a5070476a77ef7a6
Andree-Zzz <mad08061@gmail.com>
2022-06-02 22:11:59 -0500

    Prueba Deploy Heroku

--- main.py
+++ main.py
@@ -261,7 +261,7 @@ app.register_error_handler(401,status_401)
 app.register_error_handler(404,status_404)
 csrf.init_app(app)
 
-app.run(debug=True)# Comentar para desplieges a Heroku
+# app.run(debug=True)# Comentar para desplieges a Heroku
 
-if __name__ == '__main__':
-    index()
+# if __name__ == '__main__':
+#     index()
